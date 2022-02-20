43e39e278cce6f3deb37c9a01ff2f61a9b1dd772
Hwan-seok <tttkhs@naver.com>
2022-02-20 21:49:57 +0900

    constructor refactor

--- src/main/kotlin/jpabook/jpashop/domain/OrderItem.kt
+++ src/main/kotlin/jpabook/jpashop/domain/OrderItem.kt
@@ -3,28 +3,17 @@ package jpabook.jpashop.domain
 import javax.persistence.*
 
 @Entity
-class OrderItem {
-    protected constructor(
-        item: Item,
-        orderPrice: Int,
-        count: Int,
-    ) {
-        this.item = item
-        this.orderPrice = orderPrice
-        this.count = count
-    }
-
+class OrderItem protected constructor(
     @Id
     @GeneratedValue
     @Column(name = "order_item_id")
-    val id: Long? = null
-
+    val id: Long? = null,
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
-    val item: Item
-    val orderPrice: Int
-    val count: Int
-
+    val item: Item,
+    val orderPrice: Int,
+    val count: Int,
+) {
     companion object {
         fun createOrderItem(item: Item, orderPrice: Int, count: Int): OrderItem {
             val orderItem = OrderItem(
